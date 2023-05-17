eda3b187e3b7052d2ec288cfb06e69feb3453896
NikenRayaRatih <nikenrayar@gmail.com>
2023-05-17 20:55:25 +0700

    perbaikan uts

--- Main.java
+++ Main.java
@@ -10,9 +10,10 @@ class Main {
         mhs.setNim("G.111.20.0035");
         mhs.setSmt(6);
 
-        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nama Mhs: " + mhs.getNama());
         System.out.println("Nim Mhs: " + mhs.getNim());
         System.out.println("Smt Mhs: " + mhs.getSmt());
+
         System.out.println("USERNAME: " + mhs.getUsername());
         System.out.println("PASSWORD: " + mhs.getPassword());
     }
